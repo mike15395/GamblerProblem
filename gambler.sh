1d7d10acdc4c5c35f153b870c15598fe59939ee3
Mikhil Desai <mikhildesai@gmail.com>
2020-06-08 20:55:21 +0530

    checking 50% stake won or loss for singlegame

--- gambler.sh
+++ gambler.sh
@@ -6,17 +6,29 @@ loss=0
 bet=1
 amount=0
 
-
 		amount=stake
-			 # game starts
-
+		while(( amount>0 ))			 # game starts
+		do
 			check=$[(RANDOM%2)]
 
-			if (( check==1 ))			#won then adds bet amount
+			if (( check==1 ))			#won then adds bet 
 			then
 				amount=$[(amount+bet)]
-			else					#loss then subtracts bet amount
+			else							#loss then subtracts bet 
 				amount=$[(amount-bet)]
 			fi
 
+			#now check 50% stake won or loss
+			if (( amount == 150 )) 
+			then							 #winning target reached
+					((++win))
+					break;
+			elif (( amount == 50 ))
+			then							#losing condition reached
+					((++loss))
+					break;
+			fi
 
+		done		#game ends
+
+			echo $amount
