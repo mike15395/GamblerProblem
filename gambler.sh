1a50ed94ab87ac7cde2baad53bf0ba391c5d5f2c
Mikhil Desai <mikhildesai@gmail.com>
2020-06-08 20:42:33 +0530

    Win/Loss bet amount updated for single game

--- gambler.sh
+++ gambler.sh
@@ -3,12 +3,20 @@
 stake=100
 win=0
 loss=0
+bet=1
+amount=0
 
-check=$[(RANDOM%2)]
 
-	if(( check==1 ))
-	then
-		((++win))
-	else
-		((++loss))
-	fi
+		amount=stake
+			 # game starts
+
+			check=$[(RANDOM%2)]
+
+			if (( check==1 ))			#won then adds bet amount
+			then
+				amount=$[(amount+bet)]
+			else					#loss then subtracts bet amount
+				amount=$[(amount-bet)]
+			fi
+
+
